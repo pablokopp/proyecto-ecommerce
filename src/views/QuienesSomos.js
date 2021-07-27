466574cd3ce92c2ef4713283a802b742050351ff
pablokopp <84154261+pablokopp@users.noreply.github.com>
2021-07-26 23:37:53 -0400

    Desafio 6 - Prueba

--- src/views/QuienesSomos.js
+++ src/views/QuienesSomos.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import logoImg from '../components/Navbar/logo.png'
 
 export const QuienesSomos = () => {
     return (
-        <>
-        <h1>Somos miPlacard!</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor tincidunt orci, at lacinia tortor feugiat sit amet. Morbi porta mi ut lorem dictum luctus. Quisque varius, mi quis iaculis feugiat, justo massa varius lacus, et convallis nibh nisi ut magna. Nulla rhoncus commodo leo, eu imperdiet tortor mollis id. Quisque varius convallis ligula, at tempor urna ultricies eget. Nunc lacinia ipsum nec luctus interdum. Nullam sem ligula, dignissim ac aliquet eget, elementum at elit.</p>
-        </>
+    <div className='quienesSomos'>
+        <h1>Somos <span>miPlacard!</span></h1>
+        <p>
+        Eros est interdum libero, egestas ornare. Iaculis tincidunt arcu velit. Quis tortor magna fusce! Est fringilla lorem convallis etiam sociosqu viverra, in maecenas fusce tempor vestibulum. Iaculis fames rutrum cum euismod pretium dictum posuere ullamcorper a malesuada taciti quam. Tincidunt elementum suscipit facilisis facilisi fringilla nunc morbi torquent. Conubia enim sociosqu cum malesuada id metus, penatibus consequat accumsan elit mauris. Natoque litora facilisis amet at mauris erat et nec ornare? Potenti urna sed fames montes sollicitudin odio commodo nibh rutrum. Ligula malesuada purus sagittis et taciti tellus laoreet.
+        </p>
+        <p>
+        Duis enim ultricies at, tincidunt vivamus enim quis per quam morbi vitae? Mollis tristique mattis enim sem dictum id sit, praesent molestie? Potenti risus in vel hac sed natoque. Metus dolor montes commodo, eros nunc massa vitae elementum. Elit platea vulputate at convallis neque quis! Dolor dictumst cursus ultricies senectus! Inceptos proin, litora erat. Mus auctor conubia curabitur massa interdum amet congue nisl vivamus mi varius lacus! Auctor convallis purus diam sem phasellus pellentesque nostra gravida per vulputate.
+        </p>
+        <p>
+        Lorem et interdum ac pellentesque consectetur etiam parturient nostra. Lacus orci elit est habitasse cubilia porta. Venenatis penatibus velit fringilla nisi, habitasse adipiscing risus penatibus praesent? Elit ante leo tempor erat purus porttitor risus. Metus tempus fringilla ornare magnis nibh massa massa facilisis ultricies conubia. Quis proin egestas viverra scelerisque porta fringilla. Donec arcu blandit libero etiam dolor himenaeos dapibus amet habitant fringilla purus. Iaculis magna ultrices elit ornare donec placerat inceptos tempor mattis dui. Donec id suspendisse aptent quis mollis hac.
+        </p>
+        <img src={logoImg}></img>
+    </div>
     )
 }
