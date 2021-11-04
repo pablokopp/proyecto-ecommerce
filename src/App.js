ea95cbfb9582a01a1adebcadc860cfe05d5acffb
pablokopp <84154261+pablokopp@users.noreply.github.com>
2021-11-03 22:14:49 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
-import { BrowserRouter,Switch, Route } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 //Context
-import { CartProvider } from './contexts/CartContext';
+import { CartProvider } from "./contexts/CartContext";
 //Componentes
-import Navbar from './components/Navbar/Navbar';
+import Navbar from "./components/Navbar/Navbar";
 
 //Views
-import { Home } from './views/Home';
-import { QuienesSomos } from './views/QuienesSomos';
-import { Detail } from './views/Detail';
-import { Category } from './views/Category';
-import {CartView} from './views/CartView';
-import { CheckoutView } from './views/CheckoutView';
+import { Home } from "./views/Home";
+import { QuienesSomos } from "./views/QuienesSomos";
+import { Detail } from "./views/Detail";
+import { Category } from "./views/Category";
+import { CartView } from "./views/CartView";
+import { CheckoutView } from "./views/CheckoutView";
 
 function App() {
   return (
